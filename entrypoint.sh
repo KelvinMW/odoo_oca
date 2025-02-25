224858febe5cb1721a2d5289620cd3c1a5ff2ad9
KelvinM <wkelvinmaina@gmail.com>
2025-02-25 21:47:00 +0300

    OCA/OCB: include the backport repo

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,7 +6,7 @@ set -e
 : ${ODOO_CONF:=/opt/odoo/odoo.conf}
 
 # Wait for PostgreSQL
-while ! pg_isready -h $PGHOST -p $PGPORT -U $PGUSER > /dev/null 2> /dev/null; do
+while ! pg_isready -h ${PGHOST} -p ${PGPORT} -U ${PGUSER} > /dev/null 2> /dev/null; do
     echo "Waiting for PostgreSQL to start..."
     sleep 1
 done
